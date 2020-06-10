b7ea9ba0f73c27dadd7f32482b8400643d529072
Hatsanai <kongsanais@gmail.com>
2020-06-10 09:47:00 +0700

    ok commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+
+      <div @click="$router.push('/')"  class="d-flex align-center">
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn 
+        @click="$router.push('/about')"
+        target="_blank"
+        text
+        outlined
+      >
+        <span class="mr-2">About</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>  
+
+  </v-app-bar>
+    <v-content>
+            <router-view></router-view>
+    </v-content>
+  </v-app>
+
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    //
+  }),
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+   <v-container >
+     <h1>Home</h1>
+     <p class="black lighten-3 white--text">test text</p>
+     <p class="text-uppercase">test show text</p>
+     <v-btn class="primary black--text mr-5 ">run</v-btn>
+     <v-btn fab class="primary black--text mr-5">run</v-btn>
+   </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name:"home"
 }
 </script>
+
+<style>
+
+</style>
